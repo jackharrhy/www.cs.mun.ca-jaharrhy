191e108ca95d87f93eb81409746b8a062d47c495
Jack Arthur Harrhy <me@jackharrhy.com>
2019-04-17 12:12:02 -0230

    stuff

---
+++ lab/index.php
@@ -0,0 +1,27 @@
+<?php
+function get_proxy_site_page($url) {
+    $options = [
+        CURLOPT_RETURNTRANSFER => true,     // return web page
+        CURLOPT_HEADER         => false,    // return headers
+        CURLOPT_FOLLOWLOCATION => true,     // follow redirects
+        CURLOPT_ENCODING       => "",       // handle all encodings
+        CURLOPT_AUTOREFERER    => true,     // set referer on redirect
+        CURLOPT_CONNECTTIMEOUT => 120,      // timeout on connect
+        CURLOPT_TIMEOUT        => 120,      // timeout on response
+        CURLOPT_MAXREDIRS      => 10,       // stop after 10 redirects
+    ];
+
+    $ch = curl_init($url);
+    curl_setopt_array($ch, $options);
+    $remoteSite = curl_exec($ch);
+    $header = curl_getinfo($ch);
+    curl_close($ch);
+
+    $header['content'] = $remoteSite;
+
+    return $remoteSite;
+    #return $header;
+}
+
+echo get_proxy_site_page("devastator:9991/info");
+?>
