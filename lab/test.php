74ef011f6b63ce095e4fe4c360d7c837abcc5bed
Jack Arthur Harrhy <me@jackharrhy.com>
2020-02-26 00:27:52 -0330

    snoop notice

--- lab/test.php
+++ lab/test.php
@@ -1,3 +1,9 @@
+<!-- notice for those snooping around my dirs: _i mean absolutely no harm_ to labnet and wish to maintain the integrity of the uptime and data of this system, i'm just also morbidly curious -->
+
 <?php
-echo `whoami`
+?>
+
+<hr>
+
+<?php
 ?>
