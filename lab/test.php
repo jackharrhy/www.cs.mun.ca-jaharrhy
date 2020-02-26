b97c6c9bb3c64a841370de6481758c506c6d1c1d
Jack Arthur Harrhy <me@jackharrhy.com>
2020-02-26 17:12:11 -0330

    paint

--- lab/test.php
+++ lab/test.php
@@ -1,9 +1,5 @@
 <!-- notice for those snooping around my dirs: _i mean absolutely no harm_ to labnet and wish to maintain the integrity of the uptime and data of this system, i'm just also morbidly curious -->
 
 <?php
-?>
-
-<hr>
-
-<?php
+echo `sleep 1`
 ?>
