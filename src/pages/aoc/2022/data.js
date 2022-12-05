0e4856d650101230d74daec6c632be2774ea9cab
Jack Arthur Harrhy <me@jackharrhy.com>
2022-12-05 16:21:31 -0330

    add people that have done day 5 as of 4:21pm

--- src/pages/aoc/2022/data.js
+++ src/pages/aoc/2022/data.js
@@ -3,7 +3,7 @@ const pad = (num, amount = 2) => String(num).padStart(amount, "0");
 const mudkip = {
   display: "Mudkip/AdventOfCode",
   link: "https://github.com/Mudkip/AdventOfCode/",
-  days: [1, 2, 3, 4],
+  days: [1, 2, 3, 4, 5],
   language: "python",
   dayLinkFunc: (day) =>
     `https://github.com/Mudkip/AdventOfCode/blob/main/2022/day${pad(
@@ -21,7 +21,7 @@ const mat = {
 const brianna = {
   display: "briannamcdonald/advent-of-code-2022",
   link: "https://github.com/briannamcdonald/advent-of-code-2022/",
-  days: [1, 2, 3, 4],
+  days: [1, 2, 3, 4, 5],
   language: "python",
   dayLinkFunc: (day) =>
     `https://github.com/briannamcdonald/advent-of-code-2022/tree/main/day${pad(
@@ -32,7 +32,7 @@ const brianna = {
 const kent = {
   display: "krbarter/Advent-Of-Code-2022",
   link: "https://github.com/krbarter/Advent-Of-Code-2022/",
-  days: [1, 2, 3, 4],
+  days: [1, 2, 3, 4, 5],
   dayLinkFunc: (day) =>
     `https://github.com/krbarter/Advent-Of-Code-2022/blob/main/Day${day}/day${day}.py`,
   language: "python",
@@ -41,7 +41,7 @@ const kent = {
 const riley = {
   display: "nint8835/AdventOfCode2022",
   link: "https://github.com/nint8835/AdventOfCode2022/",
-  days: [1, 2, 3, 4],
+  days: [1, 2, 3, 4, 5],
   dayLinkFunc: (day) =>
     `https://github.com/nint8835/AdventOfCode2022/blob/main/Day${day}/Day${day}.fsx`,
   language: "f#",
@@ -52,6 +52,7 @@ const hamzahSheets = {
   2: "https://docs.google.com/spreadsheets/d/14X_347m2Yjt-7KavMA145m2vRu2luDlzrg73sLIsmOU/edit?usp=sharing",
   3: "https://docs.google.com/spreadsheets/d/1FV8KutkeZDnm8LlvTgG7Rb1NuVNWjU1JIc1fMJS6wBg/edit?usp=sharing",
   4: "https://docs.google.com/spreadsheets/d/1DrNkj8CXQdt0c1wrDx-_GY-QbFqmk4PDYVoNLu4ygrg/edit?usp=sharing",
+  5: "https://docs.google.com/spreadsheets/d/1cm3eHcVdDfwCe8LzPNEG1P6JpGCirheEVzDTvuJaQCc/edit?usp=sharing",
 };
 
 const hamzah = {
@@ -65,7 +66,7 @@ const hamzah = {
 const ethan = {
   display: "TheCrypticCanadian/advent-of-code-2022",
   link: "https://github.com/TheCrypticCanadian/advent-of-code-2022/",
-  days: [1, 2, 3, 4],
+  days: [1, 2, 3, 4, 5],
   dayLinkFunc: (day) =>
     `https://github.com/TheCrypticCanadian/advent-of-code-2022/tree/main/${day}`,
   language: "python",
@@ -74,7 +75,7 @@ const ethan = {
 const sven = {
   display: "STollenaar/AdventOfCode2022",
   link: "https://github.com/STollenaar/AdventOfCode2022/",
-  days: [1, 2, 3, 4],
+  days: [1, 2, 3, 4, 5],
   dayLinkFunc: (day) =>
     `https://github.com/STollenaar/AdventOfCode2022/blob/main/cmd/day${day}/main.go`,
   language: "golang",
@@ -99,7 +100,7 @@ const emily = {
 const mitch = {
   display: "ecumene/advent_of_code",
   link: "https://github.com/ecumene/advent_of_code/tree/main/2022/",
-  days: [1, 2, 3],
+  days: [1, 2, 3, 4],
   dayLinkFunc: (day) =>
     `https://github.com/ecumene/advent_of_code/blob/main/2022/notebooks/day${day}.ipynb`,
   language: "python notebook",
@@ -108,7 +109,7 @@ const mitch = {
 const zach = {
   display: "zcvaters/adventofcode2022",
   link: "https://github.com/zcvaters/adventofcode2022/",
-  days: [1, 2, 3],
+  days: [1, 2, 3, 4, 5],
   dayLinkFunc: (day) =>
     `https://github.com/zcvaters/adventofcode2022/blob/main/day${pad(
       day
@@ -128,7 +129,7 @@ const chad = {
 const devin = {
   display: "devthedevel/advent_of_code",
   link: "https://github.com/devthedevel/advent_of_code/",
-  days: [1, 2, 3],
+  days: [1, 2, 3, 4, 5],
   dayLinkFunc: (day) =>
     `https://github.com/devthedevel/advent_of_code/blob/master/2022/${day}/index.ts`,
   language: "typescript",
@@ -137,7 +138,7 @@ const devin = {
 const mathieu = {
   display: "mathieuboudreau/advent",
   link: "https://github.com/mathieuboudreau/advent/",
-  days: [1, 2, 3, 4],
+  days: [1, 2, 3, 4, 5],
   dayLinkFunc: (day) =>
     `https://github.com/mathieuboudreau/advent/tree/main/day-${pad(day)}`,
   language: "python notebook",
@@ -154,7 +155,7 @@ const steve = {
 const andrewHynes = {
   display: "ajhynes7/advent-of-code-2022",
   link: "https://github.com/ajhynes7/advent-of-code-2022/",
-  days: [1, 2, 3],
+  days: [1, 2, 3, 4, 5],
   dayLinkFunc: (day) =>
     `https://github.com/ajhynes7/advent-of-code-2022/blob/main/days/${pad(
       day
@@ -165,7 +166,7 @@ const andrewHynes = {
 const eric = {
   display: "ericthomasca/adventofcode2022",
   link: "https://github.com/ericthomasca/adventofcode2022/",
-  days: [1, 2, 3],
+  days: [1, 2, 3, 4],
   dayLinkFunc: (day) =>
     `https://github.com/ericthomasca/adventofcode2022/blob/main/day${pad(
       day
@@ -176,7 +177,7 @@ const eric = {
 const keenan = {
   display: "Keenan-Nicholson/AdventOfCode",
   link: "https://github.com/Keenan-Nicholson/AdventOfCode/",
-  days: [1, 2, 3],
+  days: [1, 2, 3, 4],
   dayLinkFunc: (day) =>
     `https://github.com/Keenan-Nicholson/AdventOfCode/blob/main/2022/day${day}/day${day}.ts`,
   language: "typescript",
@@ -194,7 +195,7 @@ const cameron = {
 const ryan = {
   display: "RyanBrushett/adventofcode2022",
   link: "https://github.com/RyanBrushett/adventofcode2022/",
-  days: [1, 2, 3, 4],
+  days: [1, 2, 3, 4, 5],
   dayLinkFunc: (day) =>
     `https://github.com/RyanBrushett/adventofcode2022/tree/main/day${day}`,
   language: "ruby",
@@ -203,7 +204,7 @@ const ryan = {
 const joel = {
   display: "joel1842/advent-of-code-2022",
   link: "https://github.com/joel1842/advent-of-code-2022/",
-  days: [1, 2, 3],
+  days: [1, 2, 3, 4],
   dayLinkFunc: (day) =>
     `https://github.com/joel1842/advent-of-code-2022/blob/main/day${day}/day${day}.py`,
   language: "python",
@@ -212,7 +213,7 @@ const joel = {
 const nathan = {
   display: "canetoads.ca",
   link: "https://canetoads.ca/",
-  days: [1, 2, 3, 4],
+  days: [1, 2, 3, 4, 5],
   dayLinkFunc: (day) => `https://canetoads.ca/day${day}.js.html`,
   language: "javascript",
 };
@@ -243,7 +244,7 @@ const ethanDenny = {
   language: "c++",
   dayLinkFunc: (day) =>
     `https://github.com/EthanDenny/AdventOfCode2022/tree/main/day${day}`,
-  days: [1, 2, 3, 4],
+  days: [1, 2, 3, 4, 5],
 };
 
 const alexis = {
@@ -252,7 +253,7 @@ const alexis = {
   language: "python",
   dayLinkFunc: (day) =>
     `https://github.com/acumpstone/advent-of-code-2022/tree/main/day${day}`,
-  days: [1, 2],
+  days: [1, 2, 3, 4],
 };
 
 const leah = {
@@ -270,7 +271,7 @@ const marty = {
   language: "rust",
   dayLinkFunc: (day) =>
     `https://github.com/lilmert/aoc/blob/main/src/years/y22/d${day}.rs`,
-  days: [1, 2, 3],
+  days: [1, 2, 3, 4, 5],
 };
 
 const brandon = {
@@ -279,7 +280,7 @@ const brandon = {
   language: "rust",
   dayLinkFunc: (day) =>
     `https://github.com/bjbemister19/AdventOfCode/blob/master/2022/day_${day}/src/main.rs`,
-  days: [1, 2, 3, 4],
+  days: [1, 2, 3, 4, 5],
 };
 
 const mohammadArafatZaman = {
@@ -287,8 +288,8 @@ const mohammadArafatZaman = {
   link: "https://github.com/M-ArafatZaman/AdventOfCode",
   dayLinkFunc: (day) =>
     `https://github.com/M-ArafatZaman/AdventOfCode/tree/main/day_${day}`,
-  language: "python",
-  days: [1, 2, 3, 4],
+  language: "python & c++",
+  days: [1, 2, 3, 4, 5],
 };
 
 const scott = {
@@ -299,7 +300,7 @@ const scott = {
       day
     )}`,
   language: "javascript",
-  days: [1, 2, 3],
+  days: [1, 2, 3, 4],
 };
 
 export const people = {
