cb0afc6ce16ee1fcad54b418c1445e0603ad2256
Jack Arthur Harrhy <me@jackharrhy.com>
2025-12-03 03:19:27 -0330

    aoc 2025 day 2 & 3

--- src/data/aoc/2025/data.ts
+++ src/data/aoc/2025/data.ts
@@ -3,7 +3,7 @@ export const pad = (num, amount = 2) => String(num).padStart(amount, "0");
 const riley = {
   display: "nint8835/advent-of-code",
   link: "https://github.com/nint8835/advent-of-code/tree/main/2025",
-  days: [1, 2],
+  days: [1, 2, 3],
   dayLinkFunc: (day) =>
     `https://github.com/nint8835/advent-of-code/blob/main/2025/${pad(
       day
@@ -14,7 +14,7 @@ const riley = {
 const mudkip = {
   display: "Mudkip/AdventOfCode",
   link: "https://github.com/Mudkip/AdventOfCode/",
-  days: [1, 2],
+  days: [1, 2, 3],
   dayLinkFunc: (day) =>
     `https://github.com/Mudkip/AdventOfCode/blob/main/2025/day${pad(
       day
@@ -36,16 +36,20 @@ const ethan = {
 const sven = {
   display: "STollenaar/AdventOfCode",
   link: "https://github.com/STollenaar/AdventOfCode/",
-  days: [1, 2],
-  dayLinkFunc: (day) =>
-    `https://github.com/STollenaar/AdventOfCode/blob/main/2025/day${day}/main.go`,
-  language: "golang",
+  days: [1, 2, 3],
+  dayLinkFunc: (day) => {
+    if (day === 3) {
+      return "https://github.com/STollenaar/AdventOfCode/blob/main/2025/day3/Day3.java";
+    }
+    return `https://github.com/STollenaar/AdventOfCode/blob/main/2025/day${day}/main.py`;
+  },
+  language: "go & java",
 };
 
 const evan = {
   display: "evaan/AdventOfCode",
   link: "https://github.com/evaan/AdventOfCode/blob/main/2025/",
-  days: [1, 2],
+  days: [1, 2, 3],
   dayLinkFunc: (day) =>
     `https://github.com/evaan/AdventOfCode/blob/main/2025/${day}/Day${day}.py`,
   language: "python",
@@ -54,7 +58,7 @@ const evan = {
 const djrideout = {
   display: "djrideout/advent2025",
   link: "https://github.com/djrideout/advent2025",
-  days: [1, 2],
+  days: [1, 2, 3],
   dayLinkFunc: (day) =>
     `https://github.com/djrideout/advent2025/blob/main/src/day${day}.rs`,
   language: "rust",
@@ -62,13 +66,13 @@ const djrideout = {
 
 const natalie = {
   display: "ncashin/aoc2025",
-  link: "https://github.com/ncashin/aoc",
+  link: "https://github.com/ncashin/Advent-Of-Code",
   days: [1],
   dayLinkFunc: (day) => {
     if (day === 1) {
       return "https://github.com/ncashin/Advent-Of-Code/blob/main/2025/day1.rb";
     }
-    return `https://github.com/ncashin/aoc/blob/main/2025/day${day}.exs`;
+    return `https://github.com/ncashin/Advent-Of-Code/blob/main/2025/day${day}.exs`;
   },
   language: "elixir",
 };
@@ -95,7 +99,7 @@ const marty = {
 const alex = {
   display: "terales/aoc-elixir",
   link: "https://github.com/terales/aoc-elixir/tree/main/2025",
-  days: [1, 2],
+  days: [1, 2, 3],
   dayLinkFunc: (day) =>
     `https://github.com/terales/aoc-elixir/blob/main/2025/d${day}/solution.exs`,
   language: "elixir",
@@ -104,12 +108,30 @@ const alex = {
 const oscar = {
   display: "OscarFKE/aoc2025",
   link: "https://github.com/OscarFKE/aoc2025",
-  days: [1],
+  days: [1, 2, 3],
   dayLinkFunc: (day) => {
     const dayStr = day.toString().padStart(2, "0");
     return `https://github.com/OscarFKE/aoc2025/blob/main/days/${dayStr}.janet`;
   },
   language: "janet",
+};
+
+const ripu = {
+  display: "singhripudaman/AOC_2025",
+  link: "https://github.com/singhripudaman/AOC_2025",
+  days: [3],
+  dayLinkFunc: (day) =>
+    `https://github.com/singhripudaman/AOC_2025/blob/main/day_${day}/solution.py`,
+  language: "python",
+};
+
+const hking = {
+  display: "hamzahap/AdventOfCode2025",
+  link: "https://github.com/hamzahap/AdventOfCode2025",
+  days: [3],
+  dayLinkFunc: (day) =>
+    `https://github.com/hamzahap/AdventOfCode2025/blob/main/Day${day}/day${day}.py`,
+  language: "python",
 };
 
 export const people = {
@@ -123,4 +145,6 @@ export const people = {
   evan,
   alex,
   oscar,
+  ripu,
+  hking,
 };
